2a00372029874b894c69a73a9ee2cfc10891c190
caro <cso@abovesports.io>
2024-04-05 12:46:37 +0200

    updating readme generator

--- generating_readme.py
+++ generating_readme.py
@@ -5,13 +5,11 @@ def generate_markdown_file():
     installation_instructions = input("Enter installation instructions for your project: ")
     usage_instructions = input("Enter usage instructions for your project: ")
     contributors = input("Enter the contributors to your project (separated by commas): ")
-    license = select_license()
 
     # Generating badges
     stars_badge = "[![GitHub stars](https://img.shields.io/github/stars/{})](https://github.com/{}/stargazers)".format(repository_name, repository_name)
     forks_badge = "[![GitHub forks](https://img.shields.io/github/forks/{})](https://github.com/{}/network/members)".format(repository_name, repository_name)
     issues_badge = "[![GitHub issues](https://img.shields.io/github/issues/{})](https://github.com/{}/issues)".format(repository_name, repository_name)
-    license_badge = "[![GitHub license](https://img.shields.io/github/license/{})](https://github.com/{}/blob/master/LICENSE)".format(repository_name, repository_name)
 
     # Generating Markdown content
     markdown_content = f"""
@@ -23,7 +21,6 @@ def generate_markdown_file():
     - [Installation](#installation)
     - [Usage](#usage)
     - [Contributors](#contributors)
-    - [License](#license)
     - [Badges](#badges)
     - [GitHub Repository](#github-repository)
 
@@ -37,8 +34,7 @@ def generate_markdown_file():
     ```
     ## Contributors
     {contributors}
-    ## License
-    This project is licensed under the {license} License - see the [LICENSE](LICENSE) file for details.
+
     ## Badges
     {stars_badge} {forks_badge} {issues_badge} {license_badge}
     ## GitHub Repository
@@ -50,22 +46,6 @@ def generate_markdown_file():
         markdown_file.write(markdown_content)
     print(f"Markdown file '{markdown_file_name}' generated successfully!")
 
-def select_license():
-    licenses = {
-        "MIT": "MIT License",
-        "Apache": "Apache License 2.0",
-        "GPL": "GNU General Public License v3.0",
-        # Add more licenses as needed
-    }
-    print("Select a license for your project:")
-    for key, value in licenses.items():
-        print(f"{key}: {value}")
-    while True:
-        selected_license = input("Enter the number corresponding to your selected license: ")
-        if selected_license in licenses:
-            return licenses[selected_license]
-        else:
-            print("Invalid input. Please enter a valid license number.")
 
 if __name__ == "__main__":
     generate_markdown_file()

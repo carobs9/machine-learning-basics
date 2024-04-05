2807ae846bec37300bc89b3656571d786619ab78
caro <cso@abovesports.io>
2024-04-05 13:27:02 +0200

    last

--- generating_readme.py
+++ generating_readme.py
@@ -1,4 +1,5 @@
 # SOURCE: https://medium.com/pythoneers/10-mindblowing-automation-scripts-you-need-to-try-using-python-8bd935f88125
+import os
 
 def generate_markdown_file():
     # Prompting user for inputs
@@ -8,6 +9,9 @@ def generate_markdown_file():
     usage_instructions = input("Enter usage instructions for your project: ")
     contributors = input("Enter the contributors to your project (separated by commas): ")
 
+    # Generating list of contents
+    contents_list = generate_contents_list()
+
     # Generating Markdown content
     markdown_content = f"""# {repository_name}
 
@@ -15,18 +19,21 @@ def generate_markdown_file():
 
 ## Table of Contents
 - [Installation](#installation)
+- [Contents](#contents)
 - [Usage](#usage)
 - [Contributors](#contributors)
 - [GitHub Repository](#github-repository)
 
 ## Installation
 {installation_instructions}
+## Contents
+{contents_list}
 ## Usage
 {usage_instructions}
 ## Contributors
 {contributors}
 ## GitHub Repository
-[Link to GitHub repository](https://github.com/{repository_name})
+[Link to GitHub repository](https://github.com/carobs9/{repository_name})
 """
 
     # Writing content to Markdown file
@@ -34,6 +41,17 @@ def generate_markdown_file():
     with open(markdown_file_name, "w") as markdown_file:
         markdown_file.write(markdown_content)
     print(f"Markdown file '{markdown_file_name}' generated successfully!")
+
+def generate_contents_list():
+    contents_list = ""
+    for root, dirs, files in os.walk("."):
+        level = root.count(os.sep)
+        indent = "    " * (level - 1)
+        contents_list += f"{indent}- **{os.path.basename(root)}/**\n"
+        sub_indent = "    " * level
+        for file in files:
+            contents_list += f"{sub_indent}- {file}\n"
+    return contents_list
 
 if __name__ == "__main__":
     generate_markdown_file()
